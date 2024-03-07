3020863f45db0f781d8ba990ee080c5c780e26b8
DonghaeSuh <gypsi12@naver.com>
2024-03-07 17:20:28 +0900

    fix - model - 학습 시 pad 토큰의 loss를 제외하지 않은 점을 수정

--- train.py
+++ train.py
@@ -12,20 +12,22 @@ from pytorch_lightning.loggers import WandbLogger
 import wandb
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, encoder_inputs, decoder_inputs):
+    def __init__(self, encoder_inputs, decoder_inputs, decoder_targets):
         self.encoder_inputs = encoder_inputs
         self.decoder_inputs = decoder_inputs
+        self.decoder_targets = decoder_targets
 
     def __getitem__(self, idx):
         return {"encoder_input_ids" : self.encoder_inputs['input_ids'][idx], 
                 "encoder_attention_mask" : self.encoder_inputs['attention_mask'][idx],
                 "decoder_input_ids" : self.decoder_inputs['input_ids'][idx],
-                "decoder_input_attention_mask" : self.decoder_inputs['attention_mask'][idx]}
+                "decoder_input_attention_mask" : self.decoder_inputs['attention_mask'][idx],
+                "decoder_target_ids" : self.decoder_targets['input_ids'][idx]}
     def __len__(self):
         return len(self.encoder_inputs['input_ids'])
     
 
-class Dataloader(torch.utils.data.DataLoader):
+class Dataloader(pl.LightningDataModule):
     def __init__(self, model_name, batch_size, shuffle, train_path, val_path, test_path, predict_path):
         super().__init__()
         self.model_name = model_name
@@ -50,33 +52,35 @@ class Dataloader(torch.utils.data.DataLoader):
     def preprocessing(self,dataframe):
         # session_dialog_add_special_tokens
         dataframe['session_dialog'] = dataframe['session_dialog'].apply(lambda x: '[BOS] ' + ' [SEP] '.join(eval(x))+ ' </s>') # ['A','B','C'] -> '[BOS] A [SEP] B [SEP] C [SEP] </s>'
-        dataframe['persona'] = dataframe['persona'].apply(lambda x: ','.join(eval(x)).replace('.','')+'.') # ['A.','B.','C.'] -> 'A,B,C.'
+        dataframe['session_persona'] = dataframe['session_persona'].apply(lambda x: '<s> '+','.join(eval(x)).replace('.','')+'.'+' </s>') # ['A.','B.','C.'] -> 'A,B,C.'
+        dataframe['session_persona_target'] =  dataframe['session_persona'].apply(lambda x: x[4:]) # <s> 제외
 
         # tokenizing
         encoder_inputs = self.tokenizing(dataframe['session_dialog'], max_len=500)
-        decoder_inputs = self.tokenizing(dataframe['persona'], max_len=200)
+        decoder_inputs = self.tokenizing(dataframe['session_persona'], max_len=200)
+        decoder_targets = self.tokenizing(dataframe['session_persona_target'], max_len=200)
 
-        return encoder_inputs, decoder_inputs
+        return encoder_inputs, decoder_inputs, decoder_targets
 
     def setup(self, stage='fit'):
         if stage == 'fit':
             train_df = pd.read_csv(self.train_path)
             val_df = pd.read_csv(self.val_path)
 
-            train_encoder_inputs, train_decoder_inputs= self.preprocessing(train_df)
-            val_encoder_inputs, val_decoder_inputs  = self.preprocessing(val_df)
+            train_encoder_inputs, train_decoder_inputs, train_decoder_targets = self.preprocessing(train_df)
+            val_encoder_inputs, val_decoder_inputs, val_decoder_targets  = self.preprocessing(val_df)
 
-            self.train_dataset = Dataset(train_encoder_inputs, train_decoder_inputs)
-            self.val_dataset = Dataset(val_encoder_inputs, val_decoder_inputs)
+            self.train_dataset = Dataset(train_encoder_inputs, train_decoder_inputs, train_decoder_targets)
+            self.val_dataset = Dataset(val_encoder_inputs, val_decoder_inputs, val_decoder_targets)
         else:
             test_df = pd.read_csv(self.test_path)
             predict_df = pd.read_csv(self.predict_path)
 
-            test_encoder_inputs, test_decoder_inputs = self.preprocessing(test_df)
-            predict_encoder_inputs, predict_decoder_inputs= self.preprocessing(predict_df)
+            test_encoder_inputs, test_decoder_inputs, test_decoder_targets= self.preprocessing(test_df)
+            predict_encoder_inputs, predict_decoder_inputs, perdict_decoder_targets = self.preprocessing(predict_df)
 
-            self.test_dataset = Dataset(test_encoder_inputs, test_decoder_inputs)
-            self.predict_dataset = Dataset(predict_encoder_inputs, predict_decoder_inputs)
+            self.test_dataset = Dataset(test_encoder_inputs, test_decoder_inputs, test_decoder_targets)
+            self.predict_dataset = Dataset(predict_encoder_inputs, predict_decoder_inputs, perdict_decoder_targets)
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=self.shuffle)
@@ -86,6 +90,7 @@ class Dataloader(torch.utils.data.DataLoader):
         return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size)
     def predict_dataloader(self):
         return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size)
+
     
 
 class Model(pl.LightningModule):
@@ -110,38 +115,38 @@ class Model(pl.LightningModule):
              "decoder_input_attention_mask" : (batch_size, decoder_max_len)}
         '''
 
-        logits = self.model(input_ids=x['encoder_input_ids'], attention_mask=x['encoder_attention_mask'], 
-                            labels=x['decoder_input_ids'], decoder_attention_mask=x['decoder_input_attention_mask']).logits
+        outputs = self.model(input_ids=x['encoder_input_ids'], attention_mask=x['encoder_attention_mask'], 
+                            decoder_input_ids=x['decoder_input_ids'], decoder_attention_mask=x['decoder_input_attention_mask'])
         
-        return logits
+        return outputs
     
     def training_step(self, batch, batch_idx):
         
-        logits = self(**batch) # (batch_size, decoder_max_len, vocab_size)
-        loss = self.loss(logits.view(-1, logits.shape[-1]), batch['decoder_input_ids'].view(-1)) # (batch_size*decoder_max_len, vocab_size), (batch_size*decoder_max_len)
+        outputs = self(**batch) # (batch_size, decoder_max_len, vocab_size)
+        loss = self.loss(outputs.logits.view(-1, outputs.logits.shape[-1]), batch['decoder_target_ids'].view(-1)) # (batch_size*decoder_max_len, vocab_size), (batch_size*decoder_max_len)
         self.log("train_loss", loss)
 
         return loss
     
     def validation_step(self, batch, batch_idx):
 
-        logits = self(**batch)
-        loss = self.loss(logits.view(-1, logits.shape[-1]), batch['decoder_input_ids'].view(-1))
+        outputs = self(**batch)
+        loss = self.loss(outputs.logits.view(-1, outputs.logits.shape[-1]), batch['decoder_input_ids'].view(-1))
         self.log("val_loss", loss)
 
-        return loss
+        return outputs.loss
     
     def test_step(self, batch, batch_idx):
             
-        logits = self(**batch)
-        loss = self.loss(logits.view(-1, logits.shape[-1]), batch['decoder_input_ids'].view(-1))
+        outputs = self(**batch)
+        # loss = self.loss(logits.view(-1, logits.shape[-1]), batch['decoder_input_ids'].view(-1))
     
-        return loss
+        return outputs.loss
     
     def predict_step(self, batch, batch_idx):
 
-        logits = self(**batch)
-        return logits
+        outputs = self(**batch)
+        return outputs.logits
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -156,10 +161,10 @@ if __name__ == '__main__':
               "learning_rate":1e-5,
               "epoch": 10,
 
-              "train_path":'./data/train.csv', 
-              "dev_path":'./data/validation.csv',
-              "test_path":'./data/validation.csv', 
-              "predict_path":'./data/validation_csv',
+              "train_path":'./data/train/train.csv', 
+              "dev_path":'./data/val/validation.csv',
+              "test_path":'./data/val/validation.csv', 
+              "predict_path":'./data/val/validation_csv',
               }
 
     wandb_logger = WandbLogger(project='persona_extraction', entity='gypsi12', name=config["model_name"] + config["model_detail"])
@@ -178,7 +183,7 @@ if __name__ == '__main__':
         monitor="val_loss",
         save_top_k=1,
         dirpath="./checkpoints/",
-        filename='_'.join(config["q_model_name"].split()+config["gen_model_name"].split() + config["model_detail"].split()), # model에 따라 변화
+        filename=config["model_name"] + config["model_detail"], # model에 따라 변화
         save_weights_only=False,
         verbose=True,
         mode="min",
